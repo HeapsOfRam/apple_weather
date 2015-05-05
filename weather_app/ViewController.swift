06e6ee592ea06aa044bb53b081388507e8b0ae5a
Ryan March <rmarch03@villanova.edu>
2015-05-05 09:28:18 -0400

    first commit

---
+++ weather_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather_app
+//
+//  Created by Ryan March on 5/5/15.
+//  Copyright (c) 2015 Ryan March. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
