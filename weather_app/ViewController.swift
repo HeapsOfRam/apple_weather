ae55d297d2ff4c7dcdbc2b32269e4cb1e5305554
Ryan March <rmarch03@villanova.edu>
2015-05-05 11:03:31 -0400

    text field hits api and prints json response

--- weather_app/ViewController.swift
+++ weather_app/ViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
+extension String{
+    func uri_encoded() -> String{
+        return self.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+    }
+}
+
 class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var city_text: UITextField!
     
     @IBAction func background_tap(sender: AnyObject) {
-        for var i=0; i<self.view.subviews.count; i+=1 {
-            self.view.subviews[i].resignFirstResponder()
+        remove_keyboard(self.view)
+    }
+    
+    @IBAction func get_weather_for_city(sender: AnyObject) {
+        let url_string = "http://api.openweathermap.org/data/2.5/weather?q=\(city_text.text)&units=imperial"
+        let url = NSURL(string: url_string.uri_encoded())!
+        let json = NSString(contentsOfURL: url, encoding: NSUTF8StringEncoding, error: nil)
+        
+        println(json)
+    }
+    
+    func remove_keyboard(container: UIView){
+        for var i=0; i<container.subviews.count; i+=1 {                    let current_view = container.subviews[i] as UIView
+            if current_view is UITextField{
+                current_view.resignFirstResponder()
+            }else if(current_view.isMemberOfClass(UIView)){
+                remove_keyboard(current_view)
+            }
         }
     }
     
